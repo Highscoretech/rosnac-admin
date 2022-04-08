1013e67b0a54248f710fa4eb374677ace801bc0d
Highscoretech <99270436+Highscoretech@users.noreply.github.com>
2022-04-08 11:59:47 +0100

    Add files via upload

---
+++ edit_acc.php
@@ -0,0 +1,22 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit account</title>
+</head>
+<body>
+    <form action="edit.php" method="post">
+        <input type="email" name="email" placeholder="enter user's email" required>
+        <input type="text" name="balance" placeholder="enter new balance" required>
+        <button type="submit" name="surbit">save</button>
+    </form>
+</body>
+</html>
+
